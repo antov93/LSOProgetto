c65cdcba8e0275a6b2286f8332c57aa95ad1223a
valerio1995 <valerio.panzera@gmail.com>
2020-01-31 16:37:03 +0100

    ultime modifiche client

--- Client/src/Client.c
+++ Client/src/Client.c
@@ -16,50 +16,55 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <sys/types.h>
+#include <string.h>
 
 #define PORTA 1205
 
 int main(void) {
-    printf("STO QUI 0");
+    
 	struct sockaddr_in serverDescriptor; //struct in cui inserisco le info del server a cui voglio connettermi
 										 //poiche il client per connettersi deve conoscere ip e porta del server
 
-
+    char buffer[1000];
 	int socketClientDescriptor; //descrittore del socket locale del client
-    printf("STO QUI 1");
+    
 	//struttura alla quale mi connetto(il server cioè)
 	serverDescriptor.sin_family=AF_INET;
 	serverDescriptor.sin_port=htons(PORTA);
 	inet_aton("192.168.137.178", &serverDescriptor.sin_addr);
-    printf("STO QUI 2");
+    
 	//creo un socket locale per il client
 	socketClientDescriptor=socket(AF_INET,SOCK_STREAM,0);
 	if(socketClientDescriptor<0)perror("Errore creazione client socket."),exit(0);
-    printf("STO QUI 3");
+    
 	//connetto il socket client all indirizzo ip del serve
 	if( connect(socketClientDescriptor,(struct sockaddr *)&serverDescriptor,sizeof(serverDescriptor)) <0)
 		perror("Errore durante la connessione al server."),exit(0);
 	printf("!!!!!!Connesso al Server!!!!!!\n");
-
+    while(1){
 	char messaggio[100];
-	printf("scrivi un messaggio da inviare al server: ");
+	printf("\nscrivi un messaggio da inviare al server: ");
 	scanf("%s",messaggio);
-
+    printf("\nIl mio messaggio è %s\n", messaggio);
+    
 	write(socketClientDescriptor,messaggio,sizeof(messaggio));
 //----------------------------------------------------------
     int i;
 
-    char buffer[100];
+    
 	read(socketClientDescriptor,buffer,sizeof(buffer));
-    printf("-[]Messaggio ricevuto: \n");
+    printf("\n-[]Messaggio ricevuto: \n");
        
-    for(i=0; i<81; i++){
+    for(i=0; i<100; i++){
         if(i % 10 == 0){
             printf("\n");     
         }else{
                 printf("%c ", buffer[i]);
               }
     }
+
+    memset(buffer, 0, sizeof(buffer));
+    }//fine while
 //----------------------------------------------------------
 	close(socketClientDescriptor);
 
