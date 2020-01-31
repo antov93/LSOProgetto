b9f24497f3f8566a028c98d3488dd4f2cf199940
valerio1995 <valerio.panzera@gmail.com>
2020-01-31 07:33:09 +0100

    ho modificato il client scrivendo che ogni 10 elementi deve andarmi a capo. Non sono riuscito a testare il codice

--- Client/src/Client.c
+++ Client/src/Client.c
@@ -20,22 +20,22 @@
 #define PORTA 1205
 
 int main(void) {
-
-	struct sockaddr_in serverDescriptor; //strut in cui inserisco le info del server a cui voglio connettermi
+    printf("STO QUI 0");
+	struct sockaddr_in serverDescriptor; //struct in cui inserisco le info del server a cui voglio connettermi
 										 //poiche il client per connettersi deve conoscere ip e porta del server
 
 
 	int socketClientDescriptor; //descrittore del socket locale del client
-
+    printf("STO QUI 1");
 	//struttura alla quale mi connetto(il server cioè)
 	serverDescriptor.sin_family=AF_INET;
 	serverDescriptor.sin_port=htons(PORTA);
 	inet_aton("192.168.137.178", &serverDescriptor.sin_addr);
-
+    printf("STO QUI 2");
 	//creo un socket locale per il client
 	socketClientDescriptor=socket(AF_INET,SOCK_STREAM,0);
 	if(socketClientDescriptor<0)perror("Errore creazione client socket."),exit(0);
-
+    printf("STO QUI 3");
 	//connetto il socket client all indirizzo ip del serve
 	if( connect(socketClientDescriptor,(struct sockaddr *)&serverDescriptor,sizeof(serverDescriptor)) <0)
 		perror("Errore durante la connessione al server."),exit(0);
@@ -49,12 +49,12 @@ int main(void) {
 //----------------------------------------------------------
     int i;
 
-    char buffer[81];
+    char buffer[100];
 	read(socketClientDescriptor,buffer,sizeof(buffer));
     printf("-[]Messaggio ricevuto: \n");
        
     for(i=0; i<81; i++){
-        if(buffer[i] == 'i'){
+        if(i % 10 == 0){
             printf("\n");     
         }else{
                 printf("%c ", buffer[i]);
