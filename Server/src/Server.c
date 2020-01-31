9292d59b3a54330c2faf6946f625f4826206fe86
valerio1995 <valerio.panzera@gmail.com>
2020-01-31 07:30:49 +0100

    la struttura del codice dovrebbe essere ok, il problema è che bisognerebbe estrarre un singolo carattere dal buffer poiché lo switch utilizza il singolo carattere. Penso di esserc riuscito ma non l'ho potuto testare

--- Server/src/Server.c
+++ Server/src/Server.c
@@ -11,15 +11,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-//#include <string.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <errno.h>
-
+#define MAX 10 //dimensione matrice
 #define PORTA 1205 //porta sulla quale il server è in ascolto
+
+void generazione(char m[][MAX]);
+void stampa(char m[][MAX]);
+void ostacoli(char m[][MAX]);
+void personaggiopacchetto(char m[][MAX]);
+int preso(char m[][MAX]);
+int ipos(char m[][MAX]);
+int jpos(char m[][MAX]);
 
 int main(int argc, char **argv) {
 
@@ -27,7 +35,12 @@ int main(int argc, char **argv) {
 	struct sockaddr_in indirizzoClient; //serve per contenere le informzioni sulla connessione che mi arrivano dal client
 	int socketDescriptor; //informazioni del server
 	int clientConnectionDescriptor; //info sul client arrivate tramite l ACCEPT
-	int ciao;
+	int i; //indice matrice
+	int j; //indice matrice
+	char m[MAX][MAX];//campo
+	char scelta;
+	int flag = 0; //flag controllo pacchetto
+
 	//creo il socket
 	socketDescriptor=socket(AF_INET,SOCK_STREAM,0);//creo il socket(famiglia,tipo,protocollo(zero per protocollo migliore disp))
 	if(socketDescriptor<0)perror("Errore creazione del socket!"),exit(1);
@@ -44,24 +57,16 @@ int main(int argc, char **argv) {
 
 	//metto in ascolto il socket(sd,lunghezzaCoda);
 	listen(socketDescriptor,1);
+    
 
-    int i, j;
+    //inizializzazione campo
+    generazione(m);
+	ostacoli(m);
+	personaggiopacchetto(m);
+
+    printf("Bisogna muoversi nel labirinto, prendere il pacchetto ed uscire per completare il gioco.\n\nLegenda: \nA-personaggio\nE-Uscita\nI-Spazio libero\nO-Ostacolo\nP-Pacchetto preso\n");
     
-    char m[9][9];
-    for(i=0; i<8; i++){
-            for(j=0; j<8; j++){
-                m[i][j] = 'I';
-                printf("%c ", m[i][j]);
-                if(j==8){
-                    m[i][j] = 'i';
-                }
-            }
-            printf("\n");
-    }
-    
-    
-    
-	while(1){
+	while(1 && m[MAX-1][MAX-1] != 'P'){
 		//accetto connessioni
 		int lunghezzaClient;
 		lunghezzaClient=sizeof(indirizzoClient);
@@ -83,10 +88,174 @@ int main(int argc, char **argv) {
 
 			printf("*Nuova connessione dal client: %s\n",inet_ntoa(indirizzoClient.sin_addr));
 			printf("In attesta di ricezione di un messaggio...\n");
+            printf("\nUtilizzare WASD per muovere il personaggio all'interno del labirinto.\n");
+            stampa(m);
 
 			char buffer[100];
 			read(clientConnectionDescriptor,buffer,sizeof(buffer));
-			printf("-[%s]Messaggio ricevuto: %s\n",inet_ntoa(indirizzoClient.sin_addr),buffer);
+
+            printf("-[%s]Messaggio ricevuto: %s\n",inet_ntoa(indirizzoClient.sin_addr),buffer);
+
+            scelta = buffer[0];
+            
+
+		switch(scelta){
+			case 'A':
+				i = ipos(m);
+				j = jpos(m);
+				flag = preso(m);
+				if(m[i][j-1] != 'O' && m[i][j-1] != 'V'){
+					if(flag == 0){
+						m[i][j] = 'I';
+						m[i][j-1] = 'A';
+					}else{
+						m[i][j] = 'I';
+						m[i][j-1] = 'P'; 				
+					}
+					
+				}else if(m[i][j-1] == 'V'){
+					m[i][j] = 'I';
+					m[i][j-1] = 'P';					
+				}
+				stampa(m);
+            break;
+            
+            case 'D':
+				i = ipos(m);
+				j = jpos(m);
+				flag = preso(m);
+				if(m[i][j+1] != 'O' && m[i][j+1] != 'V'){
+					if(flag == 0){
+						m[i][j] = 'I';
+						m[i][j+1] = 'A';
+					}else{
+						m[i][j] = 'I';
+						m[i][j+1] = 'P'; 				
+					}
+				}else if(m[i][j+1] == 'V'){
+					m[i][j] = 'I';
+					m[i][j+1] = 'P';					
+				}
+				stampa(m);
+            break;
+
+	    case 'W':
+				i = ipos(m);
+				j = jpos(m);
+				flag = preso(m);
+				if(m[i-1][j] != 'O' && m[i-1][j] != 'V'){
+					if(flag == 0){
+						m[i][j] = 'I';
+						m[i-1][j] = 'A';
+					}else{
+						m[i][j] = 'I';
+						m[i-1][j] = 'P'; 				
+					}
+				}else if(m[i-1][j] == 'V'){
+					m[i][j] = 'I';
+					m[i-1][j] = 'P';					
+				}
+				stampa(m);
+            break;
+
+	    case 'S':
+				i = ipos(m);
+				j = jpos(m);
+				flag = preso(m);
+				if(m[i+1][j] != 'O' && m[i+1][j] != 'V'){
+					if(flag == 0){
+						m[i][j] = 'I';
+						m[i+1][j] = 'A';
+					}else{
+						m[i][j] = 'I';
+						m[i+1][j] = 'P'; 				
+					}
+				}else if(m[i+1][j] == 'V'){
+					m[i][j] = 'I';
+					m[i+1][j] = 'P';					
+				}
+				stampa(m);
+            break;
+				case 'a':
+				i = ipos(m);
+				j = jpos(m);
+				flag = preso(m);
+				if(m[i][j-1] != 'O' && m[i][j-1] != 'V'){
+					if(flag == 0){
+						m[i][j] = 'I';
+						m[i][j-1] = 'A';
+					}else{
+						m[i][j] = 'I';
+						m[i][j-1] = 'P'; 				
+					}
+					
+				}else if(m[i][j-1] == 'V'){
+					m[i][j] = 'I';
+					m[i][j-1] = 'P';					
+				}
+				stampa(m);
+            break;
+            
+            case 'd':
+				i = ipos(m);
+				j = jpos(m);
+				flag = preso(m);
+				if(m[i][j+1] != 'O' && m[i][j+1] != 'V'){
+					if(flag == 0){
+						m[i][j] = 'I';
+						m[i][j+1] = 'A';
+					}else{
+						m[i][j] = 'I';
+						m[i][j+1] = 'P'; 				
+					}
+				}else if(m[i][j+1] == 'V'){
+					m[i][j] = 'I';
+					m[i][j+1] = 'P';					
+				}
+				stampa(m);
+            break;
+
+	    case 'w':
+				i = ipos(m);
+				j = jpos(m);
+				flag = preso(m);
+				if(m[i-1][j] != 'O' && m[i-1][j] != 'V'){
+					if(flag == 0){
+						m[i][j] = 'I';
+						m[i-1][j] = 'A';
+					}else{
+						m[i][j] = 'I';
+						m[i-1][j] = 'P'; 				
+					}
+				}else if(m[i-1][j] == 'V'){
+					m[i][j] = 'I';
+					m[i-1][j] = 'P';					
+				}
+				stampa(m);
+            break;
+
+	    case 's':
+				i = ipos(m);
+				j = jpos(m);
+				flag = preso(m);
+				if(m[i+1][j] != 'O' && m[i+1][j] != 'V'){
+					if(flag == 0){
+						m[i][j] = 'I';
+						m[i+1][j] = 'A';
+					}else{
+						m[i][j] = 'I';
+						m[i+1][j] = 'P'; 				
+					}
+				}else if(m[i+1][j] == 'V'){
+					m[i][j] = 'I';
+					m[i+1][j] = 'P';					
+				}
+				stampa(m);
+            break;
+			
+				}//fine switch
+
+	    			
 			printf("Attendo altre richieste...\n");
 
             write(clientConnectionDescriptor, m, 1000); //#######
@@ -100,5 +269,133 @@ int main(int argc, char **argv) {
 	}//fine while
 
 	return 0;
+
+}
+
+void generazione(char m[][MAX]){
+
+	int i;
+	int j;
+
+	for(i=0; i<MAX; i++){
+		for(j=0; j<MAX; j++){
+			m[i][j] = 'I';
+
+			}
+		}
+
+}
+
+void ostacoli(char m[][MAX]){
+	int i;
+	int j;
+	int cont = 0;
+	int casuale;
+
+	casuale=rand()%20+11;
+	while(cont<casuale){
+		
+		i=rand()%(MAX-1)+0;
+		j=rand()%(MAX-1)+0;
+	
+		if(m[i][j] == 'I'){
+			m[i][j] = 'O';
+			cont++;
+		}
+	}
+
+	m[MAX-1][MAX-1] = 'E';
+}
+
+void personaggiopacchetto(char m[][MAX]){
+	
+	int i;
+	int j;	
+	int cont = 0;
+
+	while(cont<1){
+		
+		i=rand()%(MAX-1)+0;
+		j=rand()%(MAX-1)+0;
+	
+		if(m[i][j] == 'I'){
+			m[i][j] = 'A';
+			cont++;
+		}
+	}
+
+	cont = 0;
+	while(cont<1){
+		
+		i=rand()%(MAX-1)+0;
+		j=rand()%(MAX-1)+0;
+	
+		if(m[i][j] == 'I'){
+			m[i][j] = 'V';
+			cont++;
+		}
+	}
+	
+	
+}
+
+int ipos(char m[][MAX]){
+	int i;
+	int j;
+
+	for(i=0; i<MAX; i++){
+		for(j=0; i<MAX; j++){
+			if(m[i][j] == 'A' || m[i][j] == 'P'){
+				return i;			
+			}
+
+}
+}
+}
+
+
+int jpos(char m[][MAX]){
+	int i;
+	int j;
+
+	for(i=0; i<MAX; i++){
+		for(j=0; i<MAX; j++){
+			if(m[i][j] == 'A' || m[i][j] == 'P'){
+				return j;			
+			}
+
+}
+}
+}
+
+int preso(char m[][MAX]){
+
+	int i;
+	int j;
+
+	for(i=0; i<MAX; i++){
+		for(j=0; i<MAX; j++){
+			if(m[i][j] == 'V'){
+				return 0;			
+			}else if(m[i][j] == 'P'){
+				return 1;
+			}
+	
+}
+}
+}
+ 
+void stampa(char m[][MAX]){
+
+	int i;
+	int j;
+
+	for(i=0; i<MAX; i++){
+		for(j=0; j<MAX; j++){
+			printf("%c   ", m[i][j]);
+
+			}
+			printf("\n");
+		}
 
 }
