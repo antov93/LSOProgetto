dfc3fc3f52e810c4efbf2b18e91506495da7c213
antov <you@example.com>
2020-02-06 16:03:42 +0100

    Sincronizzazione dei processi con mutex.

--- Client/src/Client.c
+++ Client/src/Client.c
@@ -17,140 +17,196 @@
 #include <netinet/ip.h>
 #include <sys/types.h>
 #include <string.h>
+#include <pthread.h>
 
-#define PORTA 1202
-#define MAX 10 
+#define PORTA 1203
+#define MAX 11
 
-void stampa(char buffer[]);
+int socketClientDescriptor; //variabile globale,descrittore del socket locale del client
+pthread_mutex_t mutex1; //servono per sincronizzare i thread 
+pthread_mutex_t mutex2;
 
-int main(int argc, char *argv[]) {
-	int i;
-	struct sockaddr_in serverDescriptor; //struct in cui inserisco le info del server a cui voglio connettermi
-								 //poiche il client per connettersi deve conoscere ip e porta del server
-    char buffer[1000]; 
-	int socketClientDescriptor; //descrittore del socket locale del client
-	char username[30];
-	char password[10];
-    char confronto[10];
-    char a;
-    int proseguo =0;
-	//struttura alla quale mi connetto(il server cioè)
-	serverDescriptor.sin_family=AF_INET;
-	serverDescriptor.sin_port=htons(PORTA);
-	inet_aton("192.168.43.219", &serverDescriptor.sin_addr);
-    
-	//creo un socket locale per il client
-	socketClientDescriptor=socket(AF_INET,SOCK_STREAM,0);
-	if(socketClientDescriptor<0)perror("Errore creazione client socket."),exit(0);
-    
-	//connetto il socket client all indirizzo ip del server
-	if( connect(socketClientDescriptor,(struct sockaddr *)&serverDescriptor,sizeof(serverDescriptor)) <0)
-		perror("Errore durante la connessione al server."),exit(0);
-	
-	printf("!!!!!!Connesso al Server!!!!!!\n");
-
-//Sistema di registrazione
- while(proseguo==0){
-    printf("\nPremere 'R' per registrarsi oppure 'L' per effettuare l'accesso\n");
-    scanf("%c", &a);
-    
-    if(a >= 'a' && a <= 'z'){
-        a = a - 32;
-    }
-
-    if(a=='R'){
-        write(socketClientDescriptor, &a, sizeof(a));
-        printf("\nScegliere l'username: \n");
-    	scanf("%s", username);
-
-	    printf("\nScegliere una password: \n");
-	    scanf("%s", password);
-
-        printf("\nRipetere password: \n");
-        scanf("%s", confronto);
-
-        if(strcmp(password,confronto) == 0){
-            char t='Y';
-            write(socketClientDescriptor, &t, sizeof(t));
-            strcat(username,password);
-            write(socketClientDescriptor, username, sizeof(username));
-        }else{
-        char t='N';
-	    printf("\nConfronto password errato, ripetere!\n");
-        write(socketClientDescriptor, &t, sizeof(t));
-	}
-        
-    }else if(a=='L'){
-
-    	write(socketClientDescriptor, &a, sizeof(a));
-    	printf("\nInserire username: \n");
-	scanf("%s", username);
-
-	printf("\nInserire password: \n");
-	scanf("%s", password);
-
-	strcat(username,password);
-
-	write(socketClientDescriptor, username, sizeof(username));
-    	proseguo=1;
-
-    }else{
-        printf("\nScelta errata, ripetere!\n");    
-    }
-
-}//fine while del sistema di registrazione	
-
-	
-	printf("\nBENVENUTO NEL GIOCO\n");
-	printf("Istruzioni:\nBisogna muoversi nel labirinto, prendere il pacchetto ed uscire per completare il gioco.\n\nLegenda: \nA-personaggio\nE-Uscita\nI-Spazio libero\nO-Ostacolo\nP-Pacchetto preso\n");
-	printf("\nUtilizzare i tasti W A S D della tastiera per muovere il personaggio all'interno del labirinto.\n");
-	
-	//leggo la matrice del gioco e visualizzo il quadro iniziale
-		printf("Quadro iniziale!\n");
-		read(socketClientDescriptor,buffer,sizeof(buffer));
+void * stampaMatriceThread(void *arg){
 		
-		//stampo la matrice
-		stampa(buffer);
+		char buffer[1000];
 		
-	while(1){
-
-	//mi metto in attesa di un nuovo messaggio/comando
-	printf("\nscrivi un messaggio da inviare al server: ");
-	scanf("%s",buffer);
-
-    //scrivo il comando sul socket
-	write(socketClientDescriptor,buffer,sizeof(buffer));
-
-    //leggo la matrice
-	read(socketClientDescriptor,buffer,sizeof(buffer));
-    printf("\n-[]Messaggio ricevuto: \n");
-       
-    //stampo la matrice
-    stampa(buffer);
-
-    }//fine while
-
-
-	return 0;
+		while(1){
+			//blocco qst codice
+			pthread_mutex_lock(&mutex1);
+			
+			//leggo la matrice del gioco e visualizzo il quadro 
+			read(socketClientDescriptor,buffer,sizeof(buffer));
+						
+			//stampo la matrice
+			for(int i=0; i<121; i++){
+				if(i % 11 == 0){
+					printf("\n"); 
+					printf("%c ", buffer[i]);
+					    
+				}else{
+					printf("%c ", buffer[i]);
+				 }
+			}
+			//sblocco il codice del th2
+			pthread_mutex_unlock(&mutex2);
+			sleep(1);
+		}
+	
+		pthread_exit(0);
 }
 
-void stampa(char buffer[]){
+void * leggiComandoThread(void *arg){
+		
+		while(1){
+			//blocco  qst codice
+			pthread_mutex_lock(&mutex2);
+			char messaggio[100];
+			//sleep(5);
+			
+			//mi metto in attesa di un nuovo messaggio/comando
+			printf("\nscrivi un messaggio da inviare al server: ");
+			scanf("%s",messaggio);
+			
+			//scrivo il comando sul socket
+			write(socketClientDescriptor,messaggio,sizeof(messaggio));
+			
+			//dopo aver mandato il comando devo leggere la nuova matrice con lo spostamento
+			//e lo faccio nel thread che stampa la matrice
+			
+			//sblocco il th1
+			pthread_mutex_unlock(&mutex1);
+			sleep(1);
+		}//fine while
+		
+		pthread_exit(0);	
+		
+}//fine funzioneThread
 
-	int i;
-	int j;
+//da cambiare con int main(int argc, char *argv[]) {  ???
+int main(void) {
+		
+		char username[30];
+		char password[10];
+	    char confronto[10];
+	    char a;
+	    int proseguo =0;
+	    
+		struct sockaddr_in serverDescriptor; //struct in cui inserisco le info del server a cui voglio connettermi
+											 //poiche il client per connettersi deve conoscere ip e porta del server
+		
+		pthread_mutex_lock(&mutex2);
+		pthread_mutex_unlock(&mutex1);
+		
+		//struttura alla quale mi connetto(il server cioè)
+		serverDescriptor.sin_family=AF_INET;
+		serverDescriptor.sin_port=htons(PORTA);
+		inet_aton("192.168.1.12", &serverDescriptor.sin_addr);
+		    
+		//creo un socket locale per il client
+		socketClientDescriptor=socket(AF_INET,SOCK_STREAM,0);
+		if(socketClientDescriptor<0)perror("Errore creazione client socket."),exit(0);
+		    
+		//connetto il socket client all indirizzo ip del server
+		if( connect(socketClientDescriptor,(struct sockaddr *)&serverDescriptor,sizeof(serverDescriptor)) <0)
+			perror("Errore durante la connessione al server."),exit(0);
+			
+		printf("!!!!!!Connesso al Server!!!!!!\n");
+		
+		//SISTEMA DI REGISTRAZIONE
+		 while(proseguo==0){
+		    printf("\nPremere 'R' per registrarsi oppure 'L' per effettuare l'accesso\n");
+		    scanf("%c", &a);
 
-	for(i=0; i<100; i++){
-       if(buffer[i] == 'o'){
-          if(i % 10 == 0){
-            printf("\n");
-          }
-          printf("i");
-        }else{
-          if(i % 10 == 0){
-            printf("\n");
-          }
-          printf("%c", buffer[i]); 
-         }
-    }
+		    if(a >= 'a' && a <= 'z'){
+		        a = a - 32;
+		    }
 
+		    if(a=='R'){
+		        write(socketClientDescriptor, &a, sizeof(a));
+		        printf("\nScegliere l'username: \n");
+		    	scanf("%s", username);
+
+			    printf("\nScegliere una password: \n");
+			    scanf("%s", password);
+
+		        printf("\nRipetere password: \n");
+		        scanf("%s", confronto);
+
+		        if(strcmp(password,confronto) == 0){
+		            char t='Y';
+		            write(socketClientDescriptor, &t, sizeof(t));
+		            strcat(username,password);
+		            write(socketClientDescriptor, username, sizeof(username));
+		        }else{
+		        	char t='N';
+		        	printf("\nConfronto password errato, ripetere!\n");
+		        	write(socketClientDescriptor, &t, sizeof(t));
+		         }
+
+		    }else if(a=='L'){
+
+		    	write(socketClientDescriptor, &a, sizeof(a));
+		    	printf("\nInserire username: \n");
+		    	scanf("%s", username);
+
+		    	printf("\nInserire password: \n");
+		    	scanf("%s", password);
+
+		    	strcat(username,password);
+
+		    	write(socketClientDescriptor, username, sizeof(username));
+		    	read(socketClientDescriptor,&proseguo,sizeof(proseguo));
+
+                if(proseguo==0){
+                    printf("\nDati errati: username o password errati!\n");
+                }
+
+		    }else{
+		        printf("\nScelta errata, ripetere!\n");    
+		    }
+
+		}//fine while del sistema di registrazione	
+
+		
+		printf("\nBENVENUTO NEL GIOCO\n");
+		printf("Istruzioni:\nBisogna muoversi nel labirinto, prendere il pacchetto ed uscire per completare il gioco.\n\nLegenda: \nA-personaggio\nE-Uscita\nI-Spazio libero\nO-Ostacolo\nP-Pacchetto preso\n");
+		printf("\nUtilizzare i tasti W A S D della tastiera per muovere il personaggio all'interno del labirinto.\n");
+		printf("Campo di gioco:\n");
+		
+		pthread_t th1,th2;
+	  
+		if( pthread_create(&th1, NULL, stampaMatriceThread, NULL) != 0 )
+			printf("Failed to create thread 1\n");
+		
+		
+		
+		if( pthread_create(&th2, NULL, leggiComandoThread, NULL) != 0 )
+			printf("Failed to create thread 2\n");
+	
+		
+		
+		pthread_join(th1,NULL);
+		pthread_join(th2,NULL);
+	
+		return 0;
 }
+
+//void stampa(char buffer[]){
+//
+//	int i;
+//	int j;
+//
+//	for(i=0; i<100; i++){
+//	       if(buffer[i] == 'o'){
+//	          if(i % 10 == 0){
+//	        	  printf("\n");
+//	          }
+//	          printf("* ");
+//	       }else{
+//	        	if(i % 10 == 0){
+//	        		printf("\n");
+//	        	}
+//	        	printf("%c ", buffer[i]); 
+//	         }
+//	}
+//}
--- Server/src/Server.c
+++ Server/src/Server.c
@@ -7,7 +7,8 @@
  Description : Uso dei Socket TCP in C
  ============================================================================
  */
-//comestai
+
+//compilare usando gcc -pthread nomefile.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -18,20 +19,213 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <errno.h>
-#define PORTA 1202
-#define MAX 10 //dimensione matrice
+#include <pthread.h>
 
-void generazione(char m[][MAX]);
-void stampa(char m[][MAX]);
-void ostacoli(char m[][MAX]);
-void personaggiopacchetto(char m[][MAX]);
+#define MAX 11 //dimensione matrice
+#define PORTA 1203 //porta sulla quale il server è in ascolto
+//#define CODA 10//client massimi che possono accedere
+
+typedef struct{
+	int descrittore;
+	char indirizzo[100];
+}parametriClient;
+
+parametriClient p[15]; //variabile globale che rappresenta tutti i client che si connettono
+int numeroClient=0; //variabile globale,indice dei client connessi
+char campoGioco[MAX][MAX]; //variabile globale, tutti i client lavorano sullo stesso campo 
+
+void generazione(char m[MAX][MAX]);
+void stampa(char m[MAX][MAX]);
+void ostacoli(char m[MAX][MAX]);
+void personaggiopacchetto(char m[MAX][MAX]);
 int preso(char m[][MAX]);
 int ipos(char m[][MAX]);
 int jpos(char m[][MAX]);
 int check(char utente[]);
 void registra(char utente[]);
 
-int main(int argc, char **argv){
+
+
+void *funzioneThread(void * arg){//devo passare  in arg il client connectiondescriptor,la matrice ,indirizzo
+	
+    		char buffer[1000];
+    		int clientThread=numeroClient-1; //client associato a qst thread
+    		char username[30];
+    		int esiste=0;//controllo dei dati utenti
+    		char accesso;
+    		int proseguo =0;//sistema di registrazione
+  			
+    		
+    		printf("**Nuova connessione dal client: %s\n",p[clientThread].indirizzo);
+  			
+    		
+    		/////REGISTRAZIONE/////
+    		while(proseguo==0){
+    			    read(p[clientThread].descrittore,&accesso,sizeof(accesso));
+    		        printf("\nIo so che accesso è %c\n", accesso);
+    				if(accesso == 'L'){
+    		                read(p[clientThread].descrittore,username,sizeof(username));
+    		            	proseguo=check(username);
+				write(p[clientThread].descrittore,&proseguo, sizeof(proseguo));
+
+    		        }else if(accesso == 'R'){
+    		                char t;
+    		                read(p[clientThread].descrittore, &t,sizeof(t));
+    		                if(t=='Y'){
+    		                read(p[clientThread].descrittore,username,sizeof(username));
+    		            	registra(username);
+    		                }
+
+    		          }else{
+    		                printf("\nPremere 'L' o 'R'!\n");
+    		            }
+
+    			}//fine while del sistema di registrazione
+    		
+    		
+  			//genero personaggio e pacco,ogni client è diverso
+  			personaggiopacchetto(campoGioco);
+  			
+            //dopo la connessione con il client invio subito il quadro di gioco a tutti i client
+  			for(int i=0;i<numeroClient;i++){
+  				write(p[i].descrittore,campoGioco,1000);
+  			}
+  			
+  			printf("In attesta di ricezione di un messaggio...\n");
+  			
+            
+              
+            while(1){
+              	
+  				//poi dopo aspetto di leggere il porssimo comando
+  				read(p[clientThread].descrittore,buffer,sizeof(buffer));
+  				printf("-[%s]Messaggio ricevuto: %s\n",p[clientThread].indirizzo,buffer);
+  				
+  				int i,j,flag,scelta;
+  				scelta = buffer[0];
+  				
+  				//se il comando è minuscolo, allora lo rendo maiuscolo
+  				if(scelta >= 'a' && scelta <= 'z'){ 
+  					scelta = scelta-32; 
+  				}
+  				
+  					switch(scelta){
+  						case 'A':
+  							i = ipos(campoGioco);
+  							j = jpos(campoGioco);
+  							flag = preso(campoGioco);
+  							if(campoGioco[i][j-1] > 'b' && campoGioco[i][j-1] < 'n'){
+  								if(flag == 0){
+  									campoGioco[i][j] = '*';
+  									campoGioco[i][j-1] = 'a';
+  								}else{
+  									campoGioco[i][j] = '*';
+  									campoGioco[i][j-1] = 'p'; 				
+  								}
+  								
+  							}else if(campoGioco[i][j-1] == 'v'){
+  								campoGioco[i][j] = '*';
+  								campoGioco[i][j-1] = 'p';					
+  							}
+  		
+								for(int i=0;i<numeroClient;i++){
+									write(p[i].descrittore,campoGioco, 1000);
+								}
+  						
+							break;
+  						
+  						case 'D':
+  							i = ipos(campoGioco);
+  							j = jpos(campoGioco);
+  							flag = preso(campoGioco);
+  							if(campoGioco[i][j+1] > 'b' && campoGioco[i][j+1] < 'n'){
+  								if(flag == 0){
+  									campoGioco[i][j] = '*';
+  									campoGioco[i][j+1] = 'a';
+  								}else{
+  									campoGioco[i][j] = '*';
+  									campoGioco[i][j+1] = 'p'; 				
+  								}
+  							}else if(campoGioco[i][j+1] == 'v'){
+  								campoGioco[i][j] = '*';
+  								campoGioco[i][j+1] = 'p';					
+  							}
+  							
+  							for(int i=0;i<numeroClient;i++){
+								write(p[i].descrittore,campoGioco, 1000);
+							}
+  							
+  							break;
+  			
+  					case 'W':
+  							i = ipos(campoGioco);
+  							j = jpos(campoGioco);
+  							flag = preso(campoGioco);
+  							if(campoGioco[i-1][j] > 'b' && campoGioco[i-1][j] < 'n'){
+  								if(flag == 0){
+  									campoGioco[i][j] = '*';
+  									campoGioco[i-1][j] = 'a';
+  								}else{
+  									campoGioco[i][j] = '*';
+  									campoGioco[i-1][j] = 'p'; 				
+  								}
+  							}else if(campoGioco[i-1][j] == 'v'){
+  								campoGioco[i][j] = '*';
+  								campoGioco[i-1][j] = 'p';					
+  							}
+  							
+  							for(int i=0;i<numeroClient;i++){
+  								write(p[i].descrittore,campoGioco, 1000);
+  							}
+  							
+  						break;
+  			
+  					case 'S':
+  							i = ipos(campoGioco);
+  							j = jpos(campoGioco);
+  							flag = preso(campoGioco);
+  							if(campoGioco[i+1][j] > 'b' && campoGioco[i+1][j] < 'n'){
+  								if(flag == 0){
+  									campoGioco[i][j] = '*';
+  									campoGioco[i+1][j] = 'a';
+  								}else{
+  									campoGioco[i][j] = '*';
+  									campoGioco[i+1][j] = 'p'; 				
+  								}
+  							}else if(campoGioco[i+1][j] == 'v'){
+  								campoGioco[i][j] = '*';
+  								campoGioco[i+1][j] = 'p';					
+  							}
+  							
+  							for(int i=0;i<numeroClient;i++){
+  								write(p[i].descrittore,campoGioco, 1000);
+  							}
+  							
+  						break;
+  						
+  					case 'P':
+  						break;
+
+  					case 'T':
+  						break;
+  						
+  					default:
+  						break;
+  					
+  					}//fine switch
+  						
+  				printf("Attendo altre richieste...\n");
+  				
+            }//fine while dei comandi(cioè accetto sempre nuovi comandi finchè nn finisco il gioco)
+
+            
+  		pthread_exit(0);
+  		
+}//fine funzioneThread
+
+
+
+int main(int argc, char **argv) {
 
 	struct sockaddr_in indirizzoServer; //serve per mettersi in ascolto su una determinata porta
 	struct sockaddr_in indirizzoClient; //serve per contenere le informzioni sulla connessione che mi arrivano dal client
@@ -39,15 +233,17 @@ int main(int argc, char **argv){
 	int clientConnectionDescriptor; //info sul client arrivate tramite l ACCEPT
 	int i; //indice matrice
 	int j; //indice matrice
-	char m[MAX][MAX];//campo
+	//char m[MAX][MAX];//campo
 	char scelta;
 	int flag = 0; //flag controllo pacchetto
-    char username[30];
-   	char buffer[1000];
-	int esiste=0;//controllo dei dati utenti
-    char accesso;
-    int proseguo =0;//sistema di registrazione
+    char buffer[1000];
     
+    //genero il campo da gioco che deve essere uguale per tutti i client
+    generazione(campoGioco);
+    	
+    //genero gli ostacoli uguali per ogni client 
+    ostacoli(campoGioco);
+    	
 	//creo il socket
 	socketDescriptor=socket(AF_INET,SOCK_STREAM,0);//creo il socket(famiglia,tipo,protocollo(zero per protocollo migliore disp))
 	if(socketDescriptor<0)perror("Errore creazione del socket!"),exit(1);
@@ -61,209 +257,36 @@ int main(int argc, char **argv){
 		perror("Errore BIND:"),exit(0);
 
 	printf("In attesa di richieste da parte di un client...\n");
-
+	
 	//metto in ascolto il socket(sd,lunghezzaCoda);
-	listen(socketDescriptor,1);
-    
-	while(1 && m[MAX-1][MAX-1] != 'P'){
+	listen(socketDescriptor,10);
+	
+	while(1){
+		   
 		//accetto connessioni
 		int lunghezzaClient;
+		//int pidFiglio=0;
 		lunghezzaClient=sizeof(indirizzoClient);
 		clientConnectionDescriptor=accept(socketDescriptor,(struct sockaddr *)&indirizzoClient,&lunghezzaClient);
 		if(clientConnectionDescriptor<0){
 			perror("Errore ACCEPT del server");
 			exit(0);
 		}
-
-		//server concorrente
-		pid_t pid;
-
-		//creo un processo figlio
-		if((pid=fork())<0){
-			perror("Errore nella FORK:");
-			exit(0);
-		}else if(pid==0){//questo è quello che fa il processo figlio
-			 
-			//inizializzazione campo
-			    generazione(m);
-				ostacoli(m);
-				personaggiopacchetto(m);
-
-			close(socketDescriptor); //lavora solo con clientConnectionDescriptor
-
-			printf("**Nuova connessione dal client: %s\n",inet_ntoa(indirizzoClient.sin_addr));
-
-
-	while(proseguo==0){
-	    read(clientConnectionDescriptor,&accesso,sizeof(accesso));
-            printf("\nIo so che accesso è %c\n", accesso);
-		if(accesso == 'L'){
-                read(clientConnectionDescriptor,username,sizeof(username));
-            	esiste=check(username);
-		        proseguo=1;
-                
-            }else if(accesso == 'R'){
-                char t;
-                read(clientConnectionDescriptor, &t,sizeof(t));
-                if(t=='Y'){
-                read(clientConnectionDescriptor,username,sizeof(username));
-            	registra(username);
-                }
-
-            }else{
-                printf("\nScelta errata!\n");
-            }
-
-	}//fine while del sistema di registrazione
-
-
-           //dopo la connessione con il client invio subito il quadro di gioco
-	   write(clientConnectionDescriptor, m, 1000);
-
-	    if(esiste==1){//se il controllo dati è andato bene, il gioco diventa disponibile
-	
-            int partitaFinita=0;
-            
-            while(partitaFinita==0){
-            	
-				//poi dopo aspetto di leggere il porssimo comando
-				read(clientConnectionDescriptor,buffer,sizeof(buffer));
-				printf("-[%s]Messaggio ricevuto: %s\n",inet_ntoa(indirizzoClient.sin_addr),buffer);
-				
-				scelta = buffer[0];
-
-                if(scelta >= 'a' && scelta <= 'z'){ //se il comando è minuscolo, allora lo rendo maiuscolo
-                    scelta = scelta-32; 
-                 }
-
-					switch(scelta){
-						case 'A':
-							i = ipos(m);
-							j = jpos(m);
-							flag = preso(m);
-							if((m[i][j-1] > 'b' && m[i][j-1] < 'n')){
-								if(flag == 0){
-									m[i][j] = 'i';
-									m[i][j-1] = 'a';
-								}else{
-									m[i][j] = 'i';
-									m[i][j-1] = 'p'; 				
-								}
-								
-							}else if(m[i][j-1] == 'v'){
-								m[i][j] = 'i';
-								m[i][j-1] = 'p';					
-							}
-                            if(flag==0){
-							    write(clientConnectionDescriptor, m, 1000);
-                            }
-
-						break;
-						
-						case 'D':
-							i = ipos(m);
-							j = jpos(m);
-							flag = preso(m);
-							if(m[i][j+1] > 'b' && m[i][j+1] < 'n'){
-								if(flag == 0){
-									m[i][j] = 'i';
-									m[i][j+1] = 'a';
-								}else{
-									m[i][j] = 'i';
-									m[i][j+1] = 'p'; 				
-								}
-							}else if(m[i][j+1] == 'v'){
-								m[i][j] = 'i';
-								m[i][j+1] = 'p';					
-							}
-
-							    write(clientConnectionDescriptor, m, 1000);
-                            
-						break;
-			
-					case 'W':
-							i = ipos(m);
-							j = jpos(m);
-							flag = preso(m);
-							if(m[i-1][j] > 'b' && m[i-1][j] < 'n'){
-								if(flag == 0){
-									m[i][j] = 'i';
-									m[i-1][j] = 'a';
-								}else{
-									m[i][j] = 'i';
-									m[i-1][j] = 'p'; 				
-								}
-							}else if(m[i-1][j] == 'v'){
-								m[i][j] = 'i';
-								m[i-1][j] = 'p';					
-							}
-
-							    write(clientConnectionDescriptor, m, 1000);
-                            
-						break;
-			
-					case 'S':
-							i = ipos(m);
-							j = jpos(m);
-							flag = preso(m);
-							if(m[i+1][j] > 'b' && m[i+1][j] < 'n'){
-								if(flag == 0){
-									m[i][j] = 'i';
-									m[i+1][j] = 'a';
-								}else{
-									m[i][j] = 'i';
-									m[i+1][j] = 'p'; 				
-								}
-							}else if(m[i+1][j] == 'v'){
-								m[i][j] = 'i';
-								m[i+1][j] = 'p';					
-							}
-							    write(clientConnectionDescriptor, m, 1000);
-                            
-						break;
-
-                    case 'P':
-							
-
-						break;
-
-                    case 'T':
-							
-
-						break;
-						
-							}//fine switch
-	/*			
-	 * |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-	 * |||||||||||PARTE DA AGGIUNGERE|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\\
-				
-				//Partita terminata
-				if(pacchetto preso && posizione personaggio==m[Max-1][MAX-1]){
-					partitaFinita=1;
-				}
-					
-				//Uscita dal gioco
-				if(partitaFinita==1){
-					printf("Bravo hai vinto una banana!");
-					exit(0);
-				}
-		||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\
-		||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||		
-	*/
-						
-				printf("Attendo altre richieste...\n");
-
-            }//fine while dei comandi(cioè accetto sempre nuovi comandi finchè nn finisco il gioco)
-
-	    }else{
-		printf("\nErrore durante l'accesso: username e/o password errati!");
-	    }//fine controllo del sistema di registrazione
-
-		 
-		}//fine else if processo figlio
-            
 		
-		close(clientConnectionDescriptor);
+		
+		
+		//memorizzo i dati del client connesso nella struttura globale
+		p[numeroClient].descrittore=clientConnectionDescriptor;
+		strcpy(p[numeroClient].indirizzo,inet_ntoa(indirizzoClient.sin_addr));
+		numeroClient++;
+		
+		pthread_t th;
+		//int i=0;
+		
+		pthread_create(&th,NULL,funzioneThread,NULL);
+		
+		//pthread_join(th,NULL);
+		
     
 	}//fine while delle connessioni(cioè accetto sempre connessioni)
     
@@ -271,60 +294,63 @@ int main(int argc, char **argv){
 
 }
 
-void generazione(char m[][MAX]){
-
-	int i;
-	int j;
-
+//genero una matrice 11x11 dove la riga e la colonna zero rappresentano le coordinate
+void generazione(char m[MAX][MAX]){
+	int i,j;
+	//tutte le celle sono *
 	for(i=0; i<MAX; i++){
 		for(j=0; j<MAX; j++){
-			m[i][j] = 'i';
-
-			}
+			m[i][j] = '*';
 		}
+	}
+	
+
+	m[0][0]=' ';
+	m[1][0]='A';
+	m[2][0]='B';
+	m[3][0]='C';
+	m[4][0]='D';
+	m[5][0]='E';
+	m[6][0]='F';
+	m[7][0]='G';
+	m[8][0]='H';
+	m[9][0]='I';
+	m[10][0]='L';
+	m[0][1]='0';
+	m[0][2]='1';
+	m[0][3]='2';
+	m[0][4]='3';
+	m[0][5]='4';
+	m[0][6]='5';
+	m[0][7]='6';
+	m[0][8]='7';
+	m[0][9]='8';
+	m[0][10]='9';
+
+	
+				
 
 }
 
+//genero 10 ostacoli casuali
 void ostacoli(char m[][MAX]){
 	int i;
 	int j;
 	int cont = 0;
 	int casuale;
-
+	
+	
 	casuale=rand()%20+11;
-	while(cont<casuale){
+	while(cont<10){
 		
 		i=rand()%(MAX-1)+0;
 		j=rand()%(MAX-1)+0;
 	
-		if(m[i][j] == 'i'){
+		if(m[i][j] == '*'){
 			m[i][j] = 'o';
 			cont++;
 		}
 	}
-
-    char coord[12]="ABCDEFGHI";
-    char coord2[12]="012345678";
-
-    for(i=0, j=0; i<10; i++){
-     m[i][j]=coord[i];
-    }
-
-    for(j=0, i=0; j<10; j++){
-     m[i][j]=coord2[j];
-    }
-
-    for(i=0, j=9; i<10; i++){
-     m[i][j]='o';
-    }
-
-     for(i=9, j=0; j<10; j++){
-     m[i][j]='o';
-    }
-
-    m[9][0] = 'L';
-    m[0][9] = '9';
-
 }
 
 void personaggiopacchetto(char m[][MAX]){
@@ -338,69 +364,74 @@ void personaggiopacchetto(char m[][MAX]){
 		i=rand()%(MAX-1)+0;
 		j=rand()%(MAX-1)+0;
 	
-		if(m[i][j] == 'i'){
+		if(m[i][j] == '*'){
 			m[i][j] = 'a';
 			cont++;
 		}
 	}
 
-	cont = 0;    
-	while(cont<5){
+	cont = 0;
+	while(cont<1){
 		
 		i=rand()%(MAX-1)+0;
 		j=rand()%(MAX-1)+0;
 	
-		if(m[i][j] == 'i'){
+		if(m[i][j] == '*'){
 			m[i][j] = 'v';
 			cont++;
 		}
 	}
+	
 }
 
+//ritorna la riga del personaggio
 int ipos(char m[][MAX]){
+	
 	int i;
 	int j;
 
 	for(i=0; i<MAX; i++){
-		for(j=0; i<MAX; j++){
+		for(j=0; j<MAX; j++){
 			if(m[i][j] == 'a' || m[i][j] == 'p'){
 				return i;			
 			}
 
-}
-}
+		}
+	}
 }
 
-
+//ritorna la colonna del personaggio
 int jpos(char m[][MAX]){
+	
 	int i;
 	int j;
 
 	for(i=0; i<MAX; i++){
-		for(j=0; i<MAX; j++){
+		for(j=0; j<MAX; j++){
 			if(m[i][j] == 'a' || m[i][j] == 'p'){
 				return j;			
 			}
 
-}
-}
+		}
+	}
 }
 
+//????scorre la matrice: qnd trova v ritorna 0,qnd trova p ritorna 1 
 int preso(char m[][MAX]){
 
 	int i;
 	int j;
 
 	for(i=0; i<MAX; i++){
-		for(j=0; i<MAX; j++){
+		for(j=0; j<MAX; j++){
 			if(m[i][j] == 'v'){
 				return 0;			
 			}else if(m[i][j] == 'p'){
 				return 1;
-			}
+			 }
 	
-}
-}
+		}
+	}
 }
  
 void stampa(char m[][MAX]){
@@ -410,7 +441,7 @@ void stampa(char m[][MAX]){
 
 	for(i=0; i<MAX; i++){
 		for(j=0; j<MAX; j++){
-			printf("%c   ", m[i][j]);
+			printf("%c ", m[i][j]);
 
 			}
 			printf("\n");
@@ -419,15 +450,14 @@ void stampa(char m[][MAX]){
 }
 
 int check(char utente[]){
+	
 	int i=0,j=0;
 	FILE *fp;
 	char lettura[20][20];
 	int dim;
 	
 	fp=fopen("utenti.txt","r"); //apro il file in lettura
-
 	if(fp){ 
-
 		while(!feof(fp)){
 			
 			fscanf(fp, "%s", lettura[i]);
@@ -437,37 +467,30 @@ int check(char utente[]){
 		
 	}else{
 		printf("Errore apertura file!");
-	}//fine controllo esistenza file
+	 }//fine controllo esistenza file
 	
 	fclose(fp);
 	
 	
 	int res = 0;
-
 	dim = i;
-
 	for (i = 0; i < dim; i++) //controllo riga per riga se i dati sono presenti nel file
 		if (strcmp(utente, lettura[i]) == 0){
             res = 1;
             return res;
         }
 			
-
 	  return res;
 	
 }
 
 void registra(char utente[]){
     FILE *fp;
-
     fp=fopen("utenti.txt","a"); //apro il file
-
     if(fp){
             fprintf(fp, "%s\n", utente);
     }else{
         printf("\nErrore nella scrittura del file!\n");
     }
-
     fclose(fp);
 }
-
